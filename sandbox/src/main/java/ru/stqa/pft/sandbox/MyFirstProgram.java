cb5af9bfcb91bcf1eeba9d50717c5c53622c4d96
Olgajjj <4tverka@inbox.ru>
2017-09-18 17:18:03 +0300

    +игнор+импорт библиотек

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,44 +1,48 @@
 package ru.stqa.pft.sandbox;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.*;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.math.*;
 
 public class MyFirstProgram {
 
 public static void main(String[] args) {	
-    System.out.println("Введите координаты первой точки:");
+    //System.out.println("Введите координаты первой точки:");
     int x,y;
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    /*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     String ab = null;
     try {
         ab = reader.readLine();
     } catch (IOException e) {
         e.printStackTrace();
     }
-    x = Integer.parseInt(ab);
-    String or = null;
+    x = Integer.parseInt(ab);*/
+    x=1;
+    /*String or = null;
     try {
         or = reader.readLine();
     } catch (IOException e) {
         e.printStackTrace();
     }
-    y = Integer.parseInt(or);
+    y = Integer.parseInt(or);*/
+    y=1;
     Point pointA = new Point(x,y);
-    System.out.println("Введите координаты второй точки:");
+    /*System.out.println("Введите координаты второй точки:");
     try {
         ab = reader.readLine();
     } catch (IOException e) {
         e.printStackTrace();
     }
-    x = Integer.parseInt(ab);
-    try {
+    x = Integer.parseInt(ab);*/
+    x=5;
+    /*try {
         or = reader.readLine();
     } catch (IOException e) {
         e.printStackTrace();
     }
-    y = Integer.parseInt(or);
+    y = Integer.parseInt(or);*/
+    y=4;
     Point pointB = new Point(x,y);
     System.out.println("Расстояние между точками "+pointA+" и "+pointB+" равно");
     System.out.print("Вычисление через функцию: ");
