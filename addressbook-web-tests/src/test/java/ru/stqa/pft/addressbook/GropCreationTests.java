94a798607fc2a9f7697ef78d642dd6e524fed72d
Olgajjj <4tverka@inbox.ru>
2017-09-19 17:26:40 +0300

    рабочий тест 1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GropCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GropCreationTests.java
@@ -26,8 +26,13 @@ public class GropCreationTests {
     @Test
     public void GropCreationTests() {
         wd.get("http://localhost/addressbook/");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).sendKeys("\\undefined");
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
