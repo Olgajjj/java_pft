73ce0ef9efaf3fc690d8bdb33a13857a20f462c4
Olgajjj <4tverka@inbox.ru>
2017-09-21 12:39:12 +0300

    +тесты

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -5,10 +5,23 @@ import org.testng.annotations.Test;
 
 public class PointTests {
 
+    Point pa = new Point(1,1);
+    Point pb = new Point(5,4);
+
     @Test
     public void testArea() {
-        Point pa = new Point(1,1);
-        Point pb = new Point(5,4);
         Assert.assertEquals(pa.calcDistance(pb),5.0);
     }
+
+    @Test
+    public void notNegativ() {
+        Assert.assertFalse(pa.calcDistance(pb)<0);
+    }
+
+    @Test
+    public void triangleTheorem() {
+        int sum = Math.abs(pa.x-pb.x) + Math.abs(pa.y-pb.y);
+        Assert.assertFalse(sum<=pa.calcDistance(pb));
+    }
+
 }
