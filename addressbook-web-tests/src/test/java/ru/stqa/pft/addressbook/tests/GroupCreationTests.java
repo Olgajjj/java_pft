0fb3d38caefd5ac7f21a16e3d78b59f1a629f40b
Olgajjj <4tverka@inbox.ru>
2017-11-21 09:43:30 +0300

    fluent интерфейс

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -14,7 +14,7 @@ public class GroupCreationTests extends TestBase {
         app.goTo().groupPage();
         List<GroupData> before = app.group().list();
         //int before = app.group().getGroupCount();
-        GroupData group = new GroupData("test2", null, null);
+        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
         //int after = app.group().getGroupCount();
         List<GroupData> after = app.group().list();
@@ -35,7 +35,7 @@ public class GroupCreationTests extends TestBase {
         };
         int max1 = after.stream().max(byId).get().getId();*/
 
-        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
