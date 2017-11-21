5f9b3819db398e418e4b99b2d34631dbba8b67ba
Olgajjj <4tverka@inbox.ru>
2017-11-21 10:04:28 +0300

    убрала расшифровку лямбд

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,28 +13,10 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.goTo().groupPage();
         List<GroupData> before = app.group().list();
-        //int before = app.group().getGroupCount();
         GroupData group = new GroupData().withName("test2");
         app.group().create(group);
-        //int after = app.group().getGroupCount();
         List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size()+1);
-
-        /*int max = 0;
-        for (GroupData g:after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }*/
-
-        /*Comparator<? super GroupData> byId = new Comparator<GroupData>() {
-            @Override
-            public int compare(GroupData o1, GroupData o2) {
-                return Integer.compare(o1.getId(), o2.getId());
-            }
-        };
-        int max1 = after.stream().max(byId).get().getId();*/
-
         group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
